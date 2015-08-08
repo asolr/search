63ea287e9c34757a713a193ef2bf2ee4946fca57
Asher Martin <ashermartin@gmail.com>
2015-08-08 13:09:46 -0400

    Open Graph Search

---
+++ layout.js
@@ -0,0 +1,59 @@
+// Search Graph Visualize
+
+function main() {
+
+  var width = 960, height = 500;
+  var color = d3.scale.category20();
+
+  var force = d3.layout.force()
+  .charge(-120)
+  .linkDistance(30)
+  .size([width, height]);
+
+  var svg = d3.select("#proximity").append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+// load json data into d3
+d3.json("data.json", function(error, graph) {
+  force
+  .nodes(graph.nodes)
+  .links(graph.links)
+  .start();
+
+  var link = svg.selectAll(".link")
+  .data(graph.links)
+  .enter().append("line")
+  .attr("class", "link")
+  .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+  var node = svg.selectAll(".node")
+  .data(graph.nodes)
+  .enter().append("circle")
+  .attr("class", "node")
+  .attr("r", 5)
+  .style("fill", function(d) { return color(d.group); })
+  .call(force.drag);
+
+  node.append("title")
+  .text(function(d) { return d.name; });
+
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+    .attr("y1", function(d) { return d.source.y; })
+    .attr("x2", function(d) { return d.target.x; })
+    .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; });
+  });
+
+  // clickable circles
+  var circles = svg.selectAll("circle");
+  circles.on("click", update);
+
+  function update(click) {
+    var information = document.getElementsByClassName("information");
+  }
+});
+}
